1f91f7b0feda3000d4285b30452fcc3a5220c42c
dosaa7 <dosaa7@naver.com>
2022-09-07 21:01:13 +0900

    increased the space for english word from 15 to 18

--- src/main/java/com/mycom/word/Word.java
+++ src/main/java/com/mycom/word/Word.java
@@ -52,7 +52,7 @@ public class Word {
         String slevel = "";
         for(int i = 0; i < level; i ++) slevel += "*";
         String str = String.format("%-3s", slevel)
-                + String.format("%15s", word) + "  " + meaning;
+                + String.format("%18s", word) + "  " + meaning;
         return str;
     }
 }
